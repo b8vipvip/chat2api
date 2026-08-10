0f4e17b6a8b7ae08bf84a22f4a608106cbe1fe71
b8vipvip <917931227@qq.com>
2026-08-10 18:23:56 +0800

    Auto-inject v0.7 audio controllers into existing tabs

--- chrome_extension/content_bootstrap.js
+++ chrome_extension/content_bootstrap.js
@@ -1,12 +1,12 @@
 (() => {
   const baseEnsureContent = ensureContent;
 
-  ensureContent = async function ensureChat2apiContentV6(tabId) {
+  ensureContent = async function ensureChat2apiContentV7(tabId) {
     await baseEnsureContent(tabId);
     try {
       await chrome.scripting.executeScript({
         target: { tabId },
-        files: ["content_guard.js", "content_voice.js"],
+        files: ["content_guard.js", "content_voice.js", "content_voice_v2.js", "content_dictation.js"],
       });
     } catch (_) {}
     return true;
