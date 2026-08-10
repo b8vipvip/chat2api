11fc58ada18b0292cbe4130835ce85bb94bfff3f
b8vipvip <917931227@qq.com>
2026-08-10 22:06:06 +0800

    Install v0.9 production patch

--- app/entry.py
+++ app/entry.py
@@ -2,7 +2,9 @@ from .main import app
 from .voice_patch import install_voice_patch
 from .v7_patch import install_v7_patch
 from .v8_patch import install_v8_patch
+from .v9_patch import install_v9_patch
 
 install_voice_patch(app)
 install_v7_patch(app)
 install_v8_patch(app)
+install_v9_patch(app)
