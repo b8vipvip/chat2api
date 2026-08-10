daacc2b3a9694c4ea6be7f0283cf4f76498142da
b8vipvip <917931227@qq.com>
2026-08-10 20:25:35 +0800

    Inject request v2 controller on existing ChatGPT tabs

--- chrome_extension/content_bootstrap.js
+++ chrome_extension/content_bootstrap.js
@@ -1,12 +1,19 @@
 (() => {
   const baseEnsureContent = ensureContent;
 
-  ensureContent = async function ensureChat2apiContentV7(tabId) {
+  ensureContent = async function ensureChat2apiContentV8(tabId) {
     await baseEnsureContent(tabId);
     try {
       await chrome.scripting.executeScript({
         target: { tabId },
-        files: ["content_guard.js", "content_voice.js", "content_voice_v2.js", "content_dictation.js"],
+        files: [
+          "content_request_v2.js",
+          "content_guard.js",
+          "content_voice.js",
+          "content_voice_v2.js",
+          "content_dictation.js",
+          "content_dictation_v3.js"
+        ],
       });
     } catch (_) {}
     return true;
