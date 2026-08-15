1cb2f70de960cf1de80fbfa1c125bf5192fbd14c
b8vipvip <917931227@qq.com>
2026-08-15 16:11:05 +0800

    Install v0.21 concurrency patch

--- app/entry.py
+++ app/entry.py
@@ -22,6 +22,7 @@ from .v20_patch import install_v20_patch
 from .v20_1_patch import install_v20_1_patch
 from .v20_2_patch import install_v20_2_patch
 from .v20_3_patch import install_v20_3_patch
+from .v21_patch import install_v21_patch
 
 install_voice_patch(app)
 install_live_voice_patch(app)
@@ -46,3 +47,4 @@ install_v20_patch(app)
 install_v20_1_patch(app)
 install_v20_2_patch(app)
 install_v20_3_patch(app)
+install_v21_patch(app)
