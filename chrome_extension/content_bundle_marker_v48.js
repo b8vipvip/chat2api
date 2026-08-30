9369c3e6144b30780f7be8bfa450e6ec665b863a
b8vipvip <917931227@qq.com>
2026-08-30 12:08:12 +0800

    Align document marker with Worker 0.8.11

--- chrome_extension/content_bundle_marker_v48.js
+++ chrome_extension/content_bundle_marker_v48.js
@@ -3,7 +3,7 @@
   if (globalThis[KEY]) return;
   globalThis[KEY] = Object.freeze({
     version: 48,
-    bundle: "0.8.10",
+    bundle: "0.8.11",
     loaded_at_ms: Date.now(),
     document_url: String(location.href || ""),
   });
