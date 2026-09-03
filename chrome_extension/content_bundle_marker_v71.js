f4722a7986b011d216899a25e5fb4ef2594705c6
b8vipvip <917931227@qq.com>
2026-09-03 10:42:45 +0800

    Align v71 bundle marker to 0.8.18

--- chrome_extension/content_bundle_marker_v71.js
+++ chrome_extension/content_bundle_marker_v71.js
@@ -1,10 +1,11 @@
 (() => {
   const KEY = "__CHAT2API_CONTENT_BUNDLE_MARKER_V71__";
   globalThis[KEY] = {
-    bundle: "0.8.17",
+    bundle: "0.8.18",
     revision: 71,
     request_revision: 69,
     response_revision: 69,
+    multimodal_revision: 78,
     installed_at_ms: Date.now(),
   };
 })();
